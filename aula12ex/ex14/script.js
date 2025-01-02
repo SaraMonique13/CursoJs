e98a35499d31253705e4233ed3ae2b29ce304c8b
Sara <moniquesarah5@gmail.com>
2025-01-02 15:11:34 -0300

    Update script.js

--- aula12ex/ex14/script.js
+++ aula12ex/ex14/script.js
@@ -1,1 +1,19 @@
-alert ('Bem vindos!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'imagens/manhã.png'
+        document.body.style.background = '#FDE687'
+    }else if(hora >= 12 && hora < 18){
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#CE8270'
+    }else{
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#04182F'
+    }
+}
