5371ea5c98a085e1304e9a9e29a312d9d3df93fb
Sara <moniquesarah5@gmail.com>
2025-01-08 15:08:09 -0300

    Update script.js

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,29 @@
-alert ('Bem vindos!')
+function gerar(){
+let num = document.getElementById('inum')
+let tab = document.getElementById('seltab')
+
+
+if(num.value.length == 0){
+    window.alert('Inválido. Digite um número!')
+}else{
+    let n = Number(num.value)
+    tab.innerHTML = ''
+    //tabuada
+    for(let c = 1; c <=10; c++){
+        let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        item.value = ` tab${c}`
+        tab.appendChild(item)
+    }
+    //let c = 1
+    //while(c <=10){
+    //let item = document.createElement('option')
+    //item.text = ` ${n} x ${c} = ${n*c}`
+    //tab.appendChild(item)
+    //c++
+    //}
+
+
+}
+
+}
