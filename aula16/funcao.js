dc4017589b8c95fd731463701fbf62596a2f83e0
Sara <moniquesarah5@gmail.com>
2025-01-09 14:43:11 -0300

    Create funcao.js

---
+++ aula16/funcao.js
@@ -0,0 +1,14 @@
+function imparPar(n){
+if(n%2 == 0){
+    return 'Par!'
+}else{
+    return 'Impar!'
+}
+}
+
+/*
+let resul = imparPar(5)
+console.log(`O valor é ${resul}`)
+*/
+
+console.log(imparPar(25))
