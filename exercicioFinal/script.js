fd7964f3482c40f99fe8e814525a049672907150
Sara <moniquesarah5@gmail.com>
2025-01-15 14:07:09 -0300

    Update script.js

--- exercicioFinal/script.js
+++ exercicioFinal/script.js
@@ -4,7 +4,7 @@ let rest = document.getElementById('res')
 let valores = []
 
 //Checar se é um número válido dentro de 1-100
-function isNumber(n){
+function isNumero(n){
 if(Number(n) >= 1 && Number(n) <= 100){
     return true
 }else{
@@ -25,15 +25,50 @@ if(l.indexOf(Number(n)) != -1){
 
 
 function adicionar(){
-if(isNumber(num.value)  && ! inLista(num.value, valores)){
+if(isNumero(num.value)  && !inLista(num.value, valores)){
 
     //valores.adicionar valor no array[]
     valores.push(Number(num.value))
-    let item = document.createAttribute('option')
+    let item = document.createElement('option')
     item.text = `O valor ${num.value} foi adicionado`
     lista.appendChild(item)
+    rest.innerHTML = ''
 
 }else{
     window.alert('Valor inválido ou já encontrado na lista')
 }
+    num.value = ''
+    num.focus()
+
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores para finalizar')
+    }else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+
+             if(valores[pos] < menor)   
+                menor = valores[pos]
+
+            }
+        
+            media = soma / total
+        rest.innerHTML = ''
+        rest.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        rest.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        rest.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        rest.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        rest.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+
+    }
 }
